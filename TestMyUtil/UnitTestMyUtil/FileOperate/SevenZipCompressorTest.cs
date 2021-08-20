19b03faea8b1c190d0582f34d09f3eac94a86da4
MinWG Lee <528952776@qq.com>
2021-08-20 22:46:53 +0800

    更新了解压文件的测试案例

---
+++ TestMyUtil/UnitTestMyUtil/FileOperate/SevenZipCompressorTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyUtil.FileOperate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtil.FileOperate
+{
+    [TestClass()]
+    public class SevenZipCompressorTest
+    {
+        [TestMethod()]
+        public void Decompress()
+        {
+            SevenZipCompressor.Decompress(@"D:\360极速浏览器下载\VisualAssistX 10.9.2358.zip", @"H:\text");
+        }
+    }
+}
