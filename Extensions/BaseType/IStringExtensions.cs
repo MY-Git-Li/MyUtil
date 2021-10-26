24b578331f74a8da7e255468161f65b8d1209d55
MinWG Lee <528952776@qq.com>
2021-10-26 12:43:55 +0800

    添加字符串简繁转换

--- Extensions/BaseType/IStringExtensions.cs
+++ Extensions/BaseType/IStringExtensions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,6 +12,35 @@ namespace MyUtil
 {
     public static class IStringExtensions
     {
+        #region 简繁转换
+
+        [DllImport("kernel32.dll", EntryPoint = "LCMapStringA")]
+        static extern int LCMapString(int Locale, int dwMapFlags, byte[] lpSrcStr, int cchSrc, byte[] lpDestStr, int cchDest);
+        const int LCMAP_SIMPLIFIED_CHINESE = 0x02000000;
+        const int LCMAP_TRADITIONAL_CHINESE = 0x04000000;
+
+        //转化方法
+        static string ToTraditional(string source, int type)
+        {
+            byte[] srcByte2 = Encoding.Default.GetBytes(source);
+            byte[] desByte2 = new byte[srcByte2.Length];
+            LCMapString(2052, type, srcByte2, -1, desByte2, srcByte2.Length);
+            string des2 = Encoding.Default.GetString(desByte2);
+            return des2;
+        }
+
+        public static string ToSimplified(this string str)
+        {
+            return ToTraditional(str, LCMAP_SIMPLIFIED_CHINESE);   //繁体转简体
+        }
+
+        public static string ToTraditional(this string str)
+        {
+            return ToTraditional(str, LCMAP_TRADITIONAL_CHINESE);   //繁体转简体
+        }
+
+        #endregion
+
         public static string Join(this IEnumerable<string> strs, string separate = ", ") => string.Join(separate, strs);
 
         #region 检测字符串中是否包含列表中的关键词
@@ -285,6 +315,9 @@ $", RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase | RegexOption
         }
     }
     #endregion
+
+
+
 
 }
 
---
+++ TestMyUtil/UnitTestMyUtil/Extensions/BaseType/IStringExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtil
+{
+    [TestClass()]
+    public class IStringExtensionsTest
+    {
+        [TestMethod()]
+        public void ToSimplifiedOrTraditional()
+        {
+            string FanStr = "大戰三國";
+            Console.WriteLine(FanStr.ToSimplified());
+
+            string janstr = "大战三国";
+            Console.WriteLine(janstr.ToTraditional());
+        }
+    }
+}
